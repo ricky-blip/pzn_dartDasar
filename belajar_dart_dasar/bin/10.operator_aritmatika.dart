153226a0aff0673c877f99bf78c3351bff4ccab2
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-26 18:28:59 +0700

    update

--- belajar_dart_dasar/bin/10.operator_aritmatika.dart
+++ belajar_dart_dasar/bin/10.operator_aritmatika.dart
@@ -1,11 +1,11 @@
 void main() {
   var first = 10;
   var second = 3;
-
+  
   print('+ plus    ${first + second}');
   print('- minus   ${first - second}');
   print('x times   ${first * second}');
   print(': double  ${first / second}');
   print(': integer ${first ~/ second}');
-  
+  print('% modulus ${first % second}');
 }
