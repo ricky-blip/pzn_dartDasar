bb3bbb637fd962dfadc254441b9b0d19a8f59391
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-27 20:05:45 +0700

    update

---
+++ belajar_dart_dasar/bin/12.operator_penugasan.dart
@@ -0,0 +1,20 @@
+void main() {
+  var a = 10;
+  
+  //a = a + 10;
+  a += 10;
+
+  print(a);
+
+  var i = 0;
+  // i++; // i = i + 1
+
+  // var j = i++; // j = i, i++
+  var k = ++i; // i++, k = i
+
+  print(i);
+  print(k);
+
+
+
+}
