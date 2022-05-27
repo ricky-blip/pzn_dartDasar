ed509af472ee94084d4b94d2c9d7058f1949881e
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-27 19:48:49 +0700

    update

---
+++ belajar_dart_dasar/bin/11.operator_perbandingan.dart
@@ -0,0 +1,11 @@
+void main() {
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('ricky' == 'ricky');
+  print('ricky' != 'ricky');
+}
