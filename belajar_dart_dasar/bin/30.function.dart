ef285b99b7c3eebed5c68a05307c69e6862b5525
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-30 18:44:18 +0700

    update

--- belajar_dart_dasar/bin/30.function.dart
+++ belajar_dart_dasar/bin/30.function.dart
@@ -10,13 +10,23 @@ void yuhuu(String firstName, String lastName) {
 
 
 //SECTION Optional Parameter []
-void halo(String city1, [String? city2, String? city3]) {
+void halo(String city1, [String? city2, String city3 = '']) { //city2 NULL city3 default value
   print('Hello Optional Parameter : $city1 $city2 $city3');
+}
+
+// SECTION Named Parameter {}
+void namedParam({String? firstHand, String? secondHand}){
+  print('Condition $firstHand $secondHand');
 }
 
 void main() {
   sayHello();
   yuhuu('Tarno', 'Nakamura');
   halo('Tokyo');
+
+  namedParam(firstHand: '1st : Iphone', secondHand: '2nd : Samsung');
+  namedParam(secondHand: '2nd : Xiaomi', firstHand: '1st : Nokia');
+  namedParam();
+
 }
 
