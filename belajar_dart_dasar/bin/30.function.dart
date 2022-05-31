24e7bd34f3018449eccaf1e7780a85f0df35ce96
ricky-blip <ricky.rinaldy77@gmail.com>
2022-05-31 23:20:15 +0700

    update

--- belajar_dart_dasar/bin/30.function.dart
+++ belajar_dart_dasar/bin/30.function.dart
@@ -15,8 +15,8 @@ void halo(String city1, [String? city2, String city3 = '']) { //city2 NULL city3
 }
 
 // SECTION Named Parameter {}
-void namedParam({String? firstHand, String? secondHand}){
-  print('Condition $firstHand $secondHand');
+void namedParam({required String newProduct, String? firstHand, String? secondHand = 'Default'}) { //newProduct (required = wajib) firstHand NULL secondHand default value
+  print('Condition | $newProduct | $firstHand $secondHand');
 }
 
 void main() {
@@ -24,9 +24,10 @@ void main() {
   yuhuu('Tarno', 'Nakamura');
   halo('Tokyo');
 
-  namedParam(firstHand: '1st : Iphone', secondHand: '2nd : Samsung');
-  namedParam(secondHand: '2nd : Xiaomi', firstHand: '1st : Nokia');
-  namedParam();
+  namedParam(newProduct: 'Motorola');
+  namedParam(firstHand: '1st : Iphone', secondHand: '2nd : Samsung', newProduct: 'Sony');
+  namedParam(secondHand: '2nd : Xiaomi', firstHand: '1st : Nokia', newProduct: 'Esia');
+
 
 }
 
