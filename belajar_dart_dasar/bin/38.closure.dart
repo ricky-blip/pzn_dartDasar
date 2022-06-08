d3be6bd8046a74ca6a84515c928ccf62b849078f
ricky-blip <ricky.rinaldy77@gmail.com>
2022-06-08 20:38:36 +0700

    update

--- belajar_dart_dasar/bin/38.closure.dart
+++ belajar_dart_dasar/bin/38.closure.dart
@@ -1,3 +1,17 @@
 void main() {
+  var counter = 0;
+
+  void increment() {
+    print('Increment++');
+    counter++;
+  }
+
+  print('This is Closure');
   
+  print(counter);
+
+  increment();
+  increment();
+
+  print(counter);
 }
